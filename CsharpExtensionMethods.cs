f259f1c4cb1a626283f8f4467f4055bd942b0117
Angelo <matheussavioti@gmail.com>
2020-12-27 03:18:21 -0300

    added foreach array extension

--- CsharpExtensionMethods.cs
+++ CsharpExtensionMethods.cs
@@ -62,5 +62,19 @@
             v1 = v2;
             v2 = temp;
         }
+
+        /// <summary>
+        /// For each component in an array, take an action
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="callback">The action to take</param>
+        public static void ForEachComponent<T>(this T[] array, System.Action<T> callback) where T : UnityEngine.Component
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                callback.Invoke(array[i]);
+            }
+        }
     }
 }
