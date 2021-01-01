9bf0dc5ccda08af6eef514e465b9b38629161b17
Angelo <matheussavioti@gmail.com>
2021-01-01 20:16:50 -0300

    adding IsBetween<T> extension

--- GenericExtensions.cs
+++ GenericExtensions.cs
@@ -1,10 +1,12 @@
-﻿namespace YoukaiFox.CsharpExtensions
+﻿using System;
+
+namespace YoukaiFox.CsharpExtensions
 {
     public static class GenericExtensions
     {
         // Author: Youkai Fox Studio
         /// <summary>
-        /// Swap the two values provided.
+        /// Swap the two values provided between themselves.
         /// </summary>
         /// <param name="v1"></param>
         /// <param name="v2"></param>
@@ -31,16 +33,6 @@
 
         // Author: Youkai Fox Studio
         /// <summary>
-        /// Returns the lesser value between <param name="v1"></param>
-        /// and <param name="v2"></param>.
-        /// </summary>
-        public static float Min(float v1, float v2)
-        {
-            return v1 < v2 ? v1 : v2;
-        }
-
-        // Author: Youkai Fox Studio
-        /// <summary>
         /// Returns the greater value between <param name="v1"></param>
         /// and <param name="v2"></param>.
         /// </summary>
@@ -51,21 +43,9 @@
 
         // Author: Youkai Fox Studio
         /// <summary>
-        /// Returns the greater value between <param name="v1"></param>
-        /// and <param name="v2"></param>.
+        /// Checks if <param name="self"></param> is present
+        /// in the collection in <param name="items"></param>
         /// </summary>
-        public static float Max(float v1, float v2)
-        {
-            return v1 > v2 ? v1 : v2;
-        }
-
-        // Author: Youkai Fox Studio
-        /// <summary>
-        /// Checks if <param name="self"> is present in <param name="items">.
-        /// </summary>
-        /// <param name="self"></param>
-        /// <param name="items"></param>
-        /// <typeparam name="T"></typeparam>
         /// <returns>Returns true if the value is present, false otherwise.</returns>
         public static bool IsIn<T>(this T self, params T[] items)
         {
@@ -73,6 +53,19 @@
                 throw new System.ArgumentNullException("self");
 
             return items.Contains(self);
+        }
+
+        // Author: From Stack Overflow.
+        /// <summary>
+        /// Checks if <param name="self"> is between <param name="lower">
+        /// (inclusive) and <param name="upper"> (exclusive).
+        /// </summary>
+        /// <typeparam name="T">Any type implementing IComparable interface.</typeparam>
+        /// <returns>Returns true if it's between the values.</returns>
+        public static bool IsBetween<T>(this T self, T lower, T upper)
+            where T : IComparable<T>
+        {
+            return self.CompareTo(lower) >= 0 && self.CompareTo(upper) < 0;
         }
     }
 }
