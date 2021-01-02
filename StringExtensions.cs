9d3626cdfb26584afef5c08df5bb3114c7870c6a
Angelo <matheussavioti@gmail.com>
2021-01-01 23:25:39 -0300

    adding IsLike string extension

--- StringExtensions.cs
+++ StringExtensions.cs
@@ -1,7 +1,39 @@
+using System;
+using System.Text.RegularExpressions;
+
 namespace YoukaiFox.CsharpExtensions
 {
     public static class StringExtensions
     {
-        
+        /// <summary>
+        /// Indicates whether the current string matches the supplied wildcard pattern.  Behaves the same
+        /// as VB's "Like" Operator.
+        /// </summary>
+        /// <param name="self">The string instance where the extension method is called</param>
+        /// <param name="wildcardPattern">The wildcard pattern to match.  Syntax matches VB's Like operator.</param>
+        /// <returns>true if the string matches the supplied pattern, false otherwise.</returns>
+        /// <remarks>See http://msdn.microsoft.com/en-us/library/swf8kaxw(v=VS.100).aspx</remarks>
+        public static bool IsLike(this string self, string wildcardPattern) {
+            if (self == null || String.IsNullOrEmpty(wildcardPattern)) return false;
+            // turn into regex pattern, and match the whole string with ^$
+            var regexPattern = "^" + Regex.Escape(wildcardPattern) + "$";
+
+            // add support for ?, #, *, [], and [!]
+            regexPattern = regexPattern.Replace(@"\[!", "[^")
+                                       .Replace(@"\[", "[")
+                                       .Replace(@"\]", "]")
+                                       .Replace(@"\?", ".")
+                                       .Replace(@"\*", ".*")
+                                       .Replace(@"\#", @"\d");
+
+            var result = false;
+            try {
+                result = Regex.IsMatch(self, regexPattern);
+            }
+            catch (ArgumentException ex) {
+                throw new ArgumentException(String.Format("Invalid pattern: {0}", wildcardPattern), ex);
+            }
+            return result;
+        }
     }
 }
